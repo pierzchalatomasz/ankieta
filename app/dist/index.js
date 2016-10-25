8ab167484c18697ed7f2446b0d3622437a994ff1
WilhelmOlejnik <wilhelmolejnik@gmail.com>
2016-10-25 10:06:23 +0200

    added MathJax

--- app/dist/index.js
+++ app/dist/index.js
@@ -375,8 +375,8 @@
 	  __esModule: {value: true}
 	});
 	var $__default = {
-	  question: 'Duis aute irure dolor in reprehenderit?',
-	  answers: ['consectetur adipiscing elit', 'sed do eiusmod tempor incididunt ut labore', 'et dolore magna aliqua'],
+	  question: 'Najciekawszy wzór matematyczny',
+	  answers: ['$$\\sqrt{2\\sqrt{2\\sqrt{2\\sqrt{\\ldots}}}}=2$$', '$$\\\infty! = \\sqrt{2 \\pi}$$', '$$\\sum_{n=1}^\\infty \\frac{1}{n^2}=\\frac{\\pi^2}{6}$$', '$$sin^{2}x+cos^{2}x=1$$', '$$e^{i\\pi}+1=0$$', '$$\\frac{2\\sqrt{2}}{99^2}\\sum_{k=0}^{\\infty} \\frac{(4k)!}{k!^4}\\frac{26390k+1103}{396^{4k}}=\\frac{1}{\\pi}$$'],
 	  correct: [0, 1]
 	};
 
