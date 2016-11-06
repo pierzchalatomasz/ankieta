c83ebd3dfbb27259c79471cebd1735aee472fb88
WilhelmOlejnik <wilhelmolejnik@gmail.com>
2016-11-06 16:17:30 +0100

    post.php and get.php added

---
+++ server_app/get.php
@@ -0,0 +1,39 @@
+<?php
+
+    $db = getDatabase();
+    $stats = getStats($db);
+    echo json_encode($stats);
+
+    function getDatabase(){
+        if(!file_exists('database.db')){
+            $db = new SQLite3('database.db');
+            createTable($db);
+        } else
+            $db = new SQLite3('database.db');
+        return $db;
+    }
+
+    function getStats($db){
+        $singleChoiceAnswers = getColSums($db, "singleChoiceAnswers");
+        $multiChoiceAnswers = getColSums($db, "multiChoiceAnswers");
+
+        return array(
+            "singleChoiceAnswers" => $singleChoiceAnswers,
+            "multiChoiceAnswers" => $multiChoiceAnswers);
+    }
+
+    function getColSums($db, $table){
+        $selectData = $db->query("
+            SELECT SUM(ans0), SUM(ans1), SUM(ans2), SUM(ans3), SUM(ans4), SUM(ans5) 
+            FROM {$table}
+        ")->fetchArray();
+
+        $colSums = array(0, 0, 0, 0, 0, 0);
+        for ($i=0; $i < count($colSums) ; $i++) 
+            $colSums[$i] = $selectData[$i];
+
+        return $colSums;
+    }
+
+
+?>
---
+++ server_app/post.php
@@ -0,0 +1,67 @@
+<?php
+ /*//TEST
+    $json = '{
+        "name":"moja stara",
+        "singleChoiceQuestion": 4,
+        "multiChoiceQuestion":[1,2,3,4,5]
+    }';
+*/
+
+    $json = $_POST["jsonFilename"];
+    $db = getDatabase();
+    saveAnswers($db, $json);
+
+    function getDatabase(){
+        if(!file_exists('database.db')){
+            $db = new SQLite3('database.db');
+            createTable($db);
+        } else
+            $db = new SQLite3('database.db');
+        return $db;
+    }
+
+    function createTable($db){
+        $tables = array('singleChoiceAnswers', 'multiChoiceAnswers');    
+        foreach( $tables as $table )
+            $db->query("
+                CREATE TABLE {$table}(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                ans0 INTEGER,
+                ans1 INTEGER,
+                ans2 INTEGER,
+                ans3 INTEGER,
+                ans4 INTEGER,
+                ans5 INTEGER);
+            ");
+    }
+
+    function saveAnswers($db, $json){
+        $submittedArray = json_decode($json);
+        $name = $submittedArray->{'name'};
+        $singleChoiceAnswer = $submittedArray->{'singleChoiceQuestion'};
+        $multiChoiceAnswers = $submittedArray->{'multiChoiceQuestion'};
+        saveSingleChoiceAnswer($db, $name, $singleChoiceAnswer);
+        saveMultiChoiceAnswers($db, $name, $multiChoiceAnswers);
+    }
+
+    function saveSingleChoiceAnswer($db, $name, $singleChoiceAnswer){
+        $answerArray = array(0, 0, 0, 0, 0, 0);
+        $answerArray[$singleChoiceAnswer] = 1;
+        insertAnswer($db, "singleChoiceAnswers", $name, $answerArray);
+    }
+
+    function saveMultiChoiceAnswers($db, $name, $multiChoiceAnswers){
+        $answerArray = array(0, 0, 0, 0, 0, 0);
+        foreach($multiChoiceAnswers as $ans)
+            $answerArray[$ans] = 1;
+        insertAnswer($db, "multiChoiceAnswers", $name, $answerArray);
+    }
+
+    function insertAnswer($db, $table, $name, $ans){
+        $db->query("
+            INSERT INTO {$table} VALUES(
+                null, '{$name}', {$ans[0]}, {$ans[1]}, {$ans[2]}, {$ans[3]}, {$ans[4]}, {$ans[5]});
+        ");
+    }
+?>
